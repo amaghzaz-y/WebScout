ceedc08f3838d328520c8506659cd22fc07554c6
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-05-30 01:24:28 +0200

    removed prisma, adopting D1 as DB

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -1,31 +1,4 @@
-import { PrismaClient, Project, User } from '@prisma/client'
 export default class DB {
-	private prisma: PrismaClient
 	constructor() {
-		this.prisma = new PrismaClient()
-	}
-	async findUserByID(id: string): Promise<User | null> {
-		const user = await this.prisma.user.findUnique({
-			where: {
-				id: id
-			}
-		})
-		return user
-	}
-	async findUserByEmail(email: string): Promise<User | null> {
-		const user = await this.prisma.user.findUnique({
-			where: {
-				email: email
-			}
-		})
-		return user
-	}
-	async findAllProjectByUserID(userID: string): Promise<Project[]> {
-		const projects = await this.prisma.project.findMany({
-			where: {
-				userId: userID
-			}
-		})
-		return projects
 	}
 }
