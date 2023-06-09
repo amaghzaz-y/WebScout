75d27d9cdd4e91d5d45823721344310b9d0d818f
Yassine AMAGHZAZ <yassine.amaghzaz@outlook.com>
2023-06-10 00:00:40 +0200

    added Crawler

--- api/src/consumers/crawler.ts
+++ api/src/consumers/crawler.ts
@@ -1,15 +1,20 @@
 import { Context } from "hono"
 import KV from "../lib/kv"
 import Spider from "../lib/spider"
-const crawler = async (c: Context, url: string, projectID: string) => {
+import QueueManager from "../lib/queue"
+
+const Crawler = async (c: Context, url: string, projectID: string) => {
 	const spider = new Spider()
 	const kv = new KV(c.env.KV)
+	const qm = new QueueManager(c.env.QUEUE_PARSER, c.env.QUEUE_INDEXER)
 	const crawledurl = await kv.getCrawledURL(projectID)
 	if (crawledurl.resources.has(url)) {
 		return
 	}
 	const urls = await spider.Crawl(url)
-	urls.forEach((url)=>{
-		
+	urls.forEach((url) => {
+		qm.SendToParser({ projectID: projectID, url: url })
 	})
 }
+
+export default Crawler
